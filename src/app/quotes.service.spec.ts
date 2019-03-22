c5b44493c2576af7a681e19fcee7a297dbc1f91c
azmizar <azmizar.ujang@outlook.com>
2019-03-22 00:27:31 -0400

    Added unit test for QuotesService

--- src/app/quotes.service.spec.ts
+++ src/app/quotes.service.spec.ts
@@ -1,12 +1,88 @@
+/**
+ * Angular imports
+ */
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpRequest } from '@angular/common/http';
 
+/**
+ * App imports
+ */
 import { QuotesService } from './quotes.service';
 
+/**
+ * Unit test for QuotesService
+ */
 describe('QuotesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpTestingController: HttpTestingController;
 
+  // setup test bed
+  beforeEach(() => { 
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: []
+    });
+
+    // Http backend mock
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  // clean up test bed
+  afterEach(() => { 
+    httpTestingController.verify();
+  });
+
+  // test instance creation
   it('should be created', () => {
     const service: QuotesService = TestBed.get(QuotesService);
     expect(service).toBeTruthy();
+  });
+
+  // test service returning a quote
+  it('should return "Quote 1"', (done) => { 
+    // get service
+    const service: QuotesService = TestBed.get(QuotesService);
+
+    // get quote
+    service.getQuote().then((val: string) => {
+      alert('Val: ' + val); 
+      expect(val).toBe('Quote 1');
+      done();
+    }).catch((err) => { 
+      alert('test: ' + err);
+      done();
+    });
+
+    // mock HTTP call
+    let data: any;
+
+    const req = httpTestingController.match((request: HttpRequest<any>): boolean => { 
+      let validReq: boolean = false;
+
+      // setup expected data based on the URL
+      if (request.url.match(/swanson/)) {
+        validReq = true;
+        data = ['Quote 1'];
+      } else if (request.url.match(/corporatebs/)) {
+        validReq = true;
+        data = { phrase: 'Quote 1' };
+      } else if (request.url.match(/geek/)) {
+        validReq = true;
+        data = 'Quote 1';
+      } else {
+        validReq = false;
+        data = null;
+      }
+
+      return ((validReq) && (request.method === 'GET'));
+    });
+
+    // expect 1 call to be made
+    expect(req.length).toBe(1);
+
+    // flush expected data
+    req[0].flush(data);
   });
 });
